aba6c68a5bcc5f5b5b74aa10087b8b710124a436
oyeon-kwon <rnjsdhdus93@gmail.com>
2021-03-06 00:39:59 +0900

    fix ch1

--- git-workflow/src/views/Nav.js
+++ git-workflow/src/views/Nav.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './Nav.css';
 import questions from '../questions.json';
+import { useHistory } from 'react-router-dom';
 
 function Nav (props) {
   const currentChapter = Number(props.currentChapter);
   const currentStep = Number(props.currentStep);
+  const history = useHistory();
+
+  const selectQuestionHandler = (q) => {
+    for(let i=0; i<questions[currentChapter].length; i++){
+      if(q.target.innerText === questions[currentChapter][i].title){
+        history.push(`/chapter${currentChapter}/${questions[currentChapter][i].number}`)
+      }
+    }
+  };
 
   return (
     <div className='nav'>
@@ -13,7 +23,7 @@ function Nav (props) {
       </div>
       <ol>
         {
-          questions[currentChapter].map(q => <li>{q.title}</li>)
+          questions[currentChapter].map((q, i) => <li className={ i + 1 === currentStep ? 'current_tab' : null } key={i} onClick={selectQuestionHandler}>{q.title}</li>)
         }
       </ol>
     </div>
