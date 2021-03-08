80117ea7122fc43769e4418311bd299a5178a77d
oyeon-kwon <rnjsdhdus93@gmail.com>
2021-03-08 14:15:49 +0900

    Add hint component

---
+++ git-workflow/src/views/Hint.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Hint.css';
+import questions from '../questions.json';
+
+function Hint (props) {
+  const currentChapter = props.currentChapter;
+  const currentStep = props.currentStep;
+
+  return (
+    <>
+      <div className='hint_modal'>{questions[currentChapter][currentStep - 1].hint}</div>
+    </>
+  );
+}
+
+export default Hint;
