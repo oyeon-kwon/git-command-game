7f640e0c49003f5f3ff038fb001bdf83ddbea41c
권오연 <61301574+oyeon-kwon@users.noreply.github.com>
2021-04-02 16:52:17 +0900

    apply semistandard lint

--- git-workflow/src/chapters/Ch2.js
+++ git-workflow/src/chapters/Ch2.js
@@ -22,7 +22,6 @@ import ch2_14 from '../imgs/ch2/ch2_14.svg';
 import ch2_15_1 from '../imgs/ch2/ch2_15_1.svg';
 import ch2_15_2 from '../imgs/ch2/ch2_15_2.svg';
 
-
 function Ch2 (props) {
   const currentStep = props.currentStep;
 
@@ -34,95 +33,95 @@ function Ch2 (props) {
           </>
         : currentStep === 2
           ? <>
-            <img src={ch2_2} className='svg show_commit' alt='git'/>
+            <img src={ch2_2} className='svg show_commit' alt='git' />
             <Remote effect='show_commit' />
             <Local effect='show_commit' localStatus='true' />
           </>
           : currentStep === 3
             ? <>
-              <img src={ch2_3} className='svg show_commit' alt='git'/>
+              <img src={ch2_3} className='svg show_commit' alt='git' />
               <Remote remoteStatus='origin-only' effect='show_commit' />
               <Local localStatus='true' />
             </>
             : currentStep === 4
               ? <>
-                <img src={ch2_4_1} className='svg' alt='git'/>
-                <img src={ch2_4_2} className='svg show_commit' alt='git'/>
+                <img src={ch2_4_1} className='svg' alt='git' />
+                <img src={ch2_4_2} className='svg show_commit' alt='git' />
                 <Remote remoteStatus='origin-only' />
                 <Local localStatus='true' />
               </>
               : currentStep === 5
                 ? <>
-                  <img src={ch2_5} className='svg' alt='git'/>
+                  <img src={ch2_5} className='svg' alt='git' />
                   <Remote remoteStatus='true' effect='show_commit' />
                   <Local localStatus='true' />
                 </>
                 : currentStep === 6
-                ? <>
-                  <img src={ch2_6} className='svg' alt='git'/>
-                  <Remote remoteStatus='true' />
-                  <Local localStatus='true' />
-                </>
-                : currentStep === 7
-                ? <>
-                  <img src={ch2_7_1} className='svg' alt='git'/>
-                  <img src={ch2_7_2} className='svg show_commit' alt='git'/>
-                  <Remote remoteStatus='true' />
-                  <Local localStatus='true' />
-                </>
-                : currentStep === 8
                   ? <>
-                    <img src={ch2_8_1} className='svg' alt='git'/>
-                    <img src={ch2_8_2} className='svg show_commit' alt='git'/>
+                    <img src={ch2_6} className='svg' alt='git' />
                     <Remote remoteStatus='true' />
                     <Local localStatus='true' />
                     </>
-                  : currentStep === 9
+                  : currentStep === 7
                     ? <>
-                      <img src={ch2_9} className='svg' alt='git'/>
+                      <img src={ch2_7_1} className='svg' alt='git' />
+                      <img src={ch2_7_2} className='svg show_commit' alt='git' />
                       <Remote remoteStatus='true' />
                       <Local localStatus='true' />
                       </>
-                    : currentStep === 10
+                    : currentStep === 8
                       ? <>
-                        <img src={ch2_10_1} className='svg' alt='git'/>
-                        <img src={ch2_10_2} className='svg show_commit' alt='git'/>
+                        <img src={ch2_8_1} className='svg' alt='git' />
+                        <img src={ch2_8_2} className='svg show_commit' alt='git' />
                         <Remote remoteStatus='true' />
                         <Local localStatus='true' />
                         </>
-                      : currentStep === 11
+                      : currentStep === 9
                         ? <>
-                        <img src={ch2_11_1} className='svg' alt='git'/>
-                        <img src={ch2_11_2} className='svg show_commit' alt='git'/>
+                          <img src={ch2_9} className='svg' alt='git' />
                           <Remote remoteStatus='true' />
                           <Local localStatus='true' />
                           </>
-                        : currentStep === 12
+                        : currentStep === 10
                           ? <>
-                          <img src={ch2_12} className='svg' alt='git'/>
+                            <img src={ch2_10_1} className='svg' alt='git' />
+                            <img src={ch2_10_2} className='svg show_commit' alt='git' />
                             <Remote remoteStatus='true' />
                             <Local localStatus='true' />
                             </>
-                          : currentStep === 13
+                          : currentStep === 11
                             ? <>
-                            <img src={ch2_13} className='svg' alt='git'/>
+                              <img src={ch2_11_1} className='svg' alt='git' />
+                              <img src={ch2_11_2} className='svg show_commit' alt='git' />
                               <Remote remoteStatus='true' />
                               <Local localStatus='true' />
                               </>
-                            : currentStep === 14
+                            : currentStep === 12
                               ? <>
-                              <img src={ch2_14} className='svg' alt='git'/>
+                                <img src={ch2_12} className='svg' alt='git' />
                                 <Remote remoteStatus='true' />
                                 <Local localStatus='true' />
                                 </>
-                              : currentStep === 15
+                              : currentStep === 13
                                 ? <>
-                                <img src={ch2_15_1} className='svg' alt='git'/>
-                                <img src={ch2_15_2} className='svg show_commit' alt='git'/>
+                                  <img src={ch2_13} className='svg' alt='git' />
                                   <Remote remoteStatus='true' />
                                   <Local localStatus='true' />
                                   </>
-                                : null
+                                : currentStep === 14
+                                  ? <>
+                                    <img src={ch2_14} className='svg' alt='git' />
+                                    <Remote remoteStatus='true' />
+                                    <Local localStatus='true' />
+                                    </>
+                                  : currentStep === 15
+                                    ? <>
+                                      <img src={ch2_15_1} className='svg' alt='git' />
+                                      <img src={ch2_15_2} className='svg show_commit' alt='git' />
+                                      <Remote remoteStatus='true' />
+                                      <Local localStatus='true' />
+                                      </>
+                                    : null
     }
     </div>
   );
