d8285aff02d99b941489cc6c2d015cd193371fd1
oyeon-kwon <rnjsdhdus93@gmail.com>
2021-06-11 01:35:40 +0900

    fix

--- git-workflow/src/chapters/Ch3.js
+++ git-workflow/src/chapters/Ch3.js
@@ -2,17 +2,15 @@ import React from 'react';
 import Remote from './layouts/Remote';
 import Local from './layouts/Local';
 import ch3_1 from '../imgs/ch3/ch3_1.svg';
-import ch3_2_1 from '../imgs/ch3/ch3_2_1.svg';
-import ch3_2_2 from '../imgs/ch3/ch3_2_2.svg';
+import ch3_2 from '../imgs/ch3/ch3_2.svg';
 import ch3_3 from '../imgs/ch3/ch3_3.svg';
 import ch3_4 from '../imgs/ch3/ch3_4.svg';
-import ch3_5_1 from '../imgs/ch3/ch3_5_1.svg';
-import ch3_5_2 from '../imgs/ch3/ch3_5_2.svg';
-import ch3_6 from '../imgs/ch3/ch3_6.svg';
+import ch3_5 from '../imgs/ch3/ch3_5.svg';
+import ch3_6_1 from '../imgs/ch3/ch3_6_1.svg';
+import ch3_6_2 from '../imgs/ch3/ch3_6_2.svg';
 import ch3_7_1 from '../imgs/ch3/ch3_7_1.svg';
 import ch3_7_2 from '../imgs/ch3/ch3_7_2.svg';
-import ch3_8_1 from '../imgs/ch3/ch3_8_1.svg';
-import ch3_8_2 from '../imgs/ch3/ch3_8_2.svg';
+import ch3_8 from '../imgs/ch3/ch3_8.svg';
 
 function Ch3 (props) {
   const currentStep = props.currentStep;
@@ -28,8 +26,7 @@ function Ch3 (props) {
           </>
         : currentStep === 2
           ? <>
-            <img src={ch3_2_1} className='svg' alt='git' />
-            <img src={ch3_2_2} className='svg show_commit' alt='git' />
+            <img src={ch3_2} className='svg' alt='git' />
             <Remote remoteStatus='project' />
             <Local localStatus='true' />
           </>
@@ -47,14 +44,14 @@ function Ch3 (props) {
             </>
               : currentStep === 5
               ? <>
-                <img src={ch3_5_1} className='svg' alt='git' />
-                <img src={ch3_5_2} className='svg show_commit' alt='git' />
+                <img src={ch3_5} className='svg' alt='git' />
                 <Remote remoteStatus='project' />
                 <Local localStatus='true' />
               </>
                 : currentStep === 6
                 ? <>
-                  <img src={ch3_6} className='svg' alt='git' />
+                  <img src={ch3_6_1} className='svg' alt='git' />
+                  <img src={ch3_6_2} className='svg show_commit' alt='git' />
                   <Remote remoteStatus='project' />
                   <Local localStatus='true' />
                 </>
@@ -67,8 +64,7 @@ function Ch3 (props) {
                   </>
                     : currentStep === 8
                     ? <>
-                      <img src={ch3_8_1} className='svg' alt='git' />
-                      <img src={ch3_8_2} className='svg show_commit' alt='git' />
+                      <img src={ch3_8} className='svg' alt='git' />
                       <Remote remoteStatus='project' />
                       <Local localStatus='true' />
                     </>
